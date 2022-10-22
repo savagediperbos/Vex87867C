78d4dcc83297831479846bcf95f293d0a4526212
why <why.coding@gmail.com>
2022-10-22 15:50:33 -0500

    Code update 10-17

--- src/driverControl.cpp
+++ src/driverControl.cpp
@@ -6,7 +6,7 @@ int data() {
     //std::cout << indexer.temperature(percent) << std::endl;
     //std::cout << flywheel.velocity(rpm) << std::endl;
     //std::cout << flywheel2.velocity(rpm) << std::endl;
-    std::cout << FL.temperature(percent) << std::endl;
+    //std::cout << FL.temperature(percent) << std::endl;
     //std::cout << Controller1.Axis4.value() << std::endl;
     //std::cout << flywheel.voltage(volt) << std::endl;
     std::cout << std::endl;
@@ -32,42 +32,33 @@ int indexerArm() {
       indexer.spin(forward, 10, volt);
       wait(95, msec);
       indexer.stop();
-    wait(25, msec);
-    }
-  }
-}
-
-bool onoff = false;
-int flywheelToggle() {
-  while (true) {
-    if (Controller1.ButtonB.pressing()) {
-      if (!onoff) {
-        onoff = true;
-        while (Controller1.ButtonB.pressing()) wait(10,msec);
-      }
-      else if (onoff) {
-        onoff = false;
-        while (Controller1.ButtonB.pressing()) wait(10,msec);
-      }
+    wait(30, msec);
     }
   }
 }
 
 int flywheelSpin() {
+  double output;
+
   double desiredRPM = 375;
   double kV = 0.017;
   double kP = 0.008;
+  int bound = 80;
 
   while (true) {
-    if (onoff) {
+    if (Controller1.ButtonB.pressing()) {
       double fVelo = -1 * flywheel.velocity(rpm);
       double fError = desiredRPM - fVelo;
-      if (fVelo < desiredRPM - 80) {
-        flywheels.spin(reverse, 12, volt);
+      if (fError > bound) {
+        output = 12;
+      }
+      else if (fError < -bound) {
+        output = 0;
       }
       else {
-        flywheels.spin(reverse, desiredRPM * kV + fError * kP, volt);
+        output = desiredRPM * kV + fError * kP;
       }
+      flywheels.spin(reverse, output, volt);
     }
     else {
       flywheels.stop();
@@ -85,31 +76,32 @@ int expand() {
 }
 
 int intake() {
-  bool stopped = true;
-  bool io = false;
+  bool ri = false;
+  bool ro = false;
   while (true) {
     if (Controller1.ButtonL1.pressing()) {
-      stopped = (stopped == true) ? false : true;
-      if (stopped == false) {
-        io = true;
+      ri = (ri == false) ? true : false;
+      if (ri == true) {
+        ro = false;
         intakeroller.spin(forward, 10, volt);
-        while (Controller1.ButtonL1.pressing()) wait(10,msec); //move to spot
       }
       else {
-        io = false;
+        ri = false;
         intakeroller.stop();
-        while (Controller1.ButtonL1.pressing()) wait(10,msec);
       }
-      //move here, use 1
+      while (Controller1.ButtonL1.pressing()) wait(10,msec);
     }
-    else if (Controller1.ButtonL2.pressing() && stopped == true) {
-      stopped = false;
-      io = false;
-      intakeroller.spin(reverse, 12, volt);
-    }
-    else if (!Controller1.ButtonL2.pressing() && stopped == false && io == false) {
-      stopped = true;
-      intakeroller.stop();
+    else if (Controller1.ButtonL2.pressing()) {
+      ro = (ro == false) ? true : false;
+      if (ro == true) {
+        ri = false;
+        intakeroller.spin(reverse, 12, volt);
+      }
+      else {
+        ro = false;
+        intakeroller.stop();
+      }
+      while (Controller1.ButtonL2.pressing()) wait(10,msec);
     }
     wait(10, msec);
   }
@@ -118,7 +110,6 @@ int intake() {
 void driverControl() {
   task t_chassis(chassis);
   task t_indexerArm(indexerArm);
-  task t_flywheelToggle(flywheelToggle);
   task t_flywheelSpin(flywheelSpin);
   task t_expand(expand);
   task t_intake(intake);
